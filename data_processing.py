11c9065edeced521c389101a1afead766cd6e12f
Kyle Smolko <ksmolko2@gmail.com>
2020-01-11 23:44:49 -0800

    Change working directory to be agnostic of machine this is run on

--- data_processing.py
+++ data_processing.py
@@ -1,8 +1,9 @@
 import os
+import sys
 import sqlite3
 import numpy as np
 
-os.chdir('/home/kyle/nwHacks/')
+os.chdir(os.path.dirname(os.path.realpath(sys.argv[0])))
 connection = sqlite3.connect('train-data/FPA_FOD_20170508.sqlite')
 cur = connection.cursor()
 cur.execute("SELECT * FROM Fires")
